f9363818768677882439562f9f7eea5b0d2e93fd
Dipesh Chaudhary <babasarkar260@gmail.com>
2025-07-02 10:39:56 +0545

    done with using locals and send and receive from client to server in ejs

--- Backend/3. 4.2 Passing Data/index.js
+++ Backend/3. 4.2 Passing Data/index.js
@@ -6,9 +6,14 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  const { fName, lName } = req.body;
+  res.render("index.ejs", { name: `${fName}${lName}` });
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
